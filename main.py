f724fa6a2fd224e2f6564c10ae1e9f4a0528010f
Олег Павлов <oleg101-101@mail.ru>
2019-12-22 22:14:14 +0300

    json support added

--- main.py
+++ main.py
@@ -1,69 +1,180 @@
-import getpass
-import sys
 from time import sleep
 import telnetlib
+import json
+
+
+def parce():
+    file = open("conf", "r")
+
+    str = file.read()
+    str = str.split(':')
+    str.pop(0)
+
+    int=str[str.index("INT")+1]
+    int=int.split(";")
+    int.pop(int.__len__()-1)
+    net=str[str.index("RIP")+1]
+    net=net.split(";")
+    vlan = str[str.index("VLAN") + 1]
+    vlan = vlan.split(";")
+    vlan.pop(vlan.__len__() - 1)
+    ospf = str[str.index("OSPF") + 1]
+    ospf = ospf.split(";")
+    ospf.pop(ospf.__len__() - 1)
+    dhcp = str[str.index("DHCP") + 1]
+    dhcp = dhcp.split(";")
+    dhcp[0]=dhcp[0][1:]
+    file.close()
+    return [int,net,vlan,ospf,dhcp]
 
 def putadata(tn):
     data = tn.read_until(b'\n').decode('ascii')
-    print(data)
+    print(data,end='')
     while (data != ''):
         data = tn.read_until(b'\n', 1).decode('ascii')
-        print(data)
+        print(data,end='')
+
+def rip(tn,net):
+    tn.write(b"conf t\n")
+    tn.write(b"router rip\n")
+    for i in net:
+
+        tn.write(b"network " + bytes(i, 'utf-8') + b"\n")
+    tn.write(b"exit\n")
+    tn.write(b"exit\n")
+
+def intconf(tn,inter):
+    tn.write(b"conf t\n")
+    putadata(tn)
+    for i in inter:
+        tn.write(b"int " + bytes(i['int'], 'utf-8') + b"\n")
+        sleep(0.01)
+        tn.write(b"ip address " + bytes(i["network"], 'utf-8') + b"\n")
+        sleep(0.01)
+        if i["no_sh"] == "1":
+            tn.write(b"no sh\n")
+            sleep(0.01)
+        putadata(tn)
+    tn.write(b"exit\n")
+    tn.write(b"exit\n")
+
+def vlanconf(tn,vlan):
+    tn.write(b"conf t\n")
+    putadata(tn)
+    for i in vlan:
+        tn.write(b"int " + bytes(i['int'], 'utf-8') + b"\n")
+        tn.write(b"encapsulation dot1q " + bytes(i['int'][i['int'].index(".")+1:], 'utf-8') + b"\n")
+        sleep(0.01)
+        tn.write(b"ip address " + bytes(i["network"], 'utf-8') + b"\n")
+        sleep(0.01)
+        if i["no_sh"] == "1":
+            tn.write(b"no sh\n")
+            sleep(0.01)
+        putadata(tn)
+    tn.write(b"exit\n")
+    tn.write(b"exit\n")
+
+def ospfconf(tn,ospf):
+    tn.write(b"conf t\n")
+    putadata(tn)
+
+    tn.write(b"router ospf " + bytes(ospf["id"], 'utf-8')+b"\n")
+    for i in range(ospf["network"].__len__()):
+        tn.write(b"network " + bytes(ospf["network"][i], 'utf-8') + b" area " + bytes(ospf["area"][i], 'utf-8') + b"\n")
+        sleep(0.01)
+        putadata(tn)
+    tn.write(b"exit\n")
+    tn.write(b"exit\n")
+
+def get_ip_route(tn,name):
+    arr=[]
+    data="d"
+    while (data != ''):
+        data = tn.read_until(b'\n', 1).decode('ascii')
+    tn.write(b"show ip route\n\t")
+    data = tn.read_until(b'\n').decode('ascii')
+    while (data != ''):
+        data = tn.read_until(b'\n', 1).decode('ascii')
+        arr.append(data)
+        #print(data, end='')
+
+    arr=arr[1:-3]
+    #print(arr)
+
+    buf=arr.index('\r\n')
+    info=arr[:buf]
+    buf2=arr.index('\r\n',buf+1)
+    gateway=arr[buf+1:buf2]
+    routes=arr[buf2+1:]
+    #print(gateway)
+    file=open(name+"_ip_route.txt","w")
+    for i in routes:
+        file.write(i[:-2]+"\n")
+    file.close()
+
+def get_int(tn,name):
+    arr=[]
+    data="d"
+    while (data != ''):
+        data = tn.read_until(b'\n', 1).decode('ascii')
+    tn.write(b"show ip int brief\n\t")
+    data = tn.read_until(b'\n').decode('ascii')
+    while (data != ''):
+        data = tn.read_until(b'\n', 1).decode('ascii')
+        arr.append(data)
+        #print(data, end='')
+
+    arr=arr[:arr.index("Router#\r\n")]
+    file=open(name+"_int.txt","w")
+    for i in arr:
+        file.write(i[:-2]+"\n")
+    file.close()
+
+def dhcpconf(tn,dhcp):
+    tn.write(b"conf t\n")
+    putadata(tn)
+    for i in dhcp["exclude"]:
+        tn.write(b"ip dhcp excluded-address " + bytes(i, 'utf-8') + b"\n")
+
+    tn.write(b"ip dhcp pool " + bytes(dhcp["name"], 'utf-8') + b"\n")
+    tn.write(b"network " + bytes(dhcp["network"], 'utf-8') + b"\n")
+    tn.write(b"default-router " + bytes(dhcp["default"], 'utf-8') + b"\n")
+    tn.write(b"dns-server " + bytes(dhcp["dns"], 'utf-8') + b"\n")
+    tn.write(b"domain-name " + bytes(dhcp["domain"], 'utf-8') + b"\n")
+    tn.write(b"end\n")
+
+
 
 HOST = '192.168.56.101'
-#user = raw_input("Enter your remote account: ")
-password = b"eve"
-user = b"root"
 port = 32769
+with open('example.json', 'r') as json_file:
+    data = json.load(json_file)
+HOST=data["host"]
+routers=data["routers"]
+for i in routers:
+    print("configurating "+i['name'])
 
-tn = telnetlib.Telnet(HOST,port,5)
-
-
-#tn.write('sh ver ')
-file=open("conf","r")
-data = ''
-#print(tn.read_until(b'\n').decode('ascii'))
-#print (data)
-#putadata(tn)
-#tn.write(b"\n")
-#tn.write(b"no\n")
-#putadata(tn)
-tn.write(b"enable\n")
-
-tn.write(b"conf t\n")
-putadata(tn)
-
-str=file.readline()
-while str!="":
-    str=str.split(';')
-    tn.write(b"int "+bytes(str[0], 'utf-8')+b"\n")
+    tn = telnetlib.Telnet(HOST,port,5)
+    tn.write(b"enable\n")
+    if (i.get("int")!=None):
+        intconf(tn,i["int"])
+    if (i.get("rip") != None):
+        rip(tn,i["rip"])
+    if (i.get("vlan") != None):
+        vlanconf(tn,i["vlan"])
+    if (i.get("ospf") != None):
+        ospfconf(tn,i["ospf"])
+    if (i.get("dhcp") != None):
+        dhcpconf(tn,i["dhcp"])
     sleep(0.01)
-    tn.write(b"ip address "+bytes(str[1], 'utf-8')+b' '+bytes(str[2], 'utf-8')+b"\n")
+
+
+    get_ip_route(tn,i['name'])
+    get_int(tn,i["name"])
+    if (i["save"]==True):
+        tn.write(b"wr\n")
+    #tn.write(b"exit\n")
     sleep(0.01)
-    if str[3]=="1\n":
-        tn.write(b"no sh\n")
-        sleep(0.01)
     putadata(tn)
-    str = file.readline()
-
-tn.write(b"exit\n")
-sleep(0.01)
-
-tn.write(b"interface Loopback 0\n")
-sleep(0.01)
-tn.write(b"ip address 10.0.0.10 255.255.255.0\n")
-sleep(0.01)
-tn.write(b"exit\n")
-sleep(0.01)
-putadata(tn)
-tn.write(b"exit\n")
-sleep(0.01)
-putadata(tn)
-
-tn.write(b"show ip route\n\t")
-putadata(tn)
-tn.write(b"exit\n")
-sleep(0.01)
-putadata(tn)
-tn.write(b"\n")
-tn.close()
+    #tn.write(b"\n")
+    tn.close()
